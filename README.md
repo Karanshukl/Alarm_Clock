18da276c45ae6d65a21ad4399db0ade0edfe7fc6
karan shukla <karnshukl143@gmail.com>
2024-09-28 17:38:37 +0530

    add files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Alarm_Clock
+# Alarm Clock for Beginners 
+
+Author - Karan Shukla
+Date - 2024-09-28
+
+
+
---
+++ main.py
@@ -0,0 +1,38 @@
+import time
+import datetime
+import pygame
+
+def setAlarm(alarmTime):
+    print(f"\nAlarm Stop: {alarmTime}")
+    sound = "Alarm_clock/Millionaire Glory 320 Kbps.mp3"
+    running = True
+    alarm_start_time = None
+
+    while running:
+        curr_time = datetime.datetime.now().strftime("%I:%M:%S %p")
+        print(f"\r\033[031m{curr_time}\033[0m", end="")
+       
+        if curr_time == alarmTime:
+            print()
+            print("Wake Up!")
+            running = False
+
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound)
+            pygame.mixer.music.play()
+            alarm_start_time = time.time()
+
+            while pygame.mixer.music.get_busy():
+                if time.time() - alarm_start_time >= 45:
+                    pygame.mixer.music.stop()
+                    break
+                
+        time.sleep(1)
+
+if __name__ == "__main__":
+    hour = int(input("Enter Hour: "))
+    minute = int(input("Enter Minutes: "))
+    seconds = int(input("Enter seconds: "))
+    symbol= input("AM / PM: ")
+    alarmTime = (f"{hour:02d}:{minute:02d}:{seconds:02d} {symbol}").upper()
+    setAlarm(alarmTime)
